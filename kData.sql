8b37f8b248fb9b0c5e8507ca0ca6e184a5ab0847
SaraMaria <sara.maria@dce.ufpb.br>
2020-11-27 08:15:53 -0300

    Ajustes

--- kData.sql
+++ kData.sql
@@ -2,62 +2,64 @@ CREATE DATABASE kData;
 USE kData;
 
 CREATE TABLE produto (
-produto_id INT(11) NOT NULL,
-status CHAR(1) NOT NULL DEFAULT 'A',
-descricao VARCHAR(50) NULL DEFAULT NULL,
-preco DECIMAL(9,2) NULL DEFAULT '0.00',
-estoque INT(11) NULL DEFAULT NULL,
-peso DECIMAL(9,2) NULL DEFAULT '0.00',
-artista VARCHAR(15) NULL DEFAULT NULL,
-id INT PRIMARY KEY AUTO_INCREMENT
+    produto_id INT(11) NOT NULL,
+    status CHAR(1) NOT NULL DEFAULT 'A',
+    descricao VARCHAR(50) NULL DEFAULT NULL,
+    preco DECIMAL(9,2) NULL DEFAULT '0.00',
+    estoque INT(11) NULL DEFAULT NULL,
+    peso DECIMAL(9,2) NULL DEFAULT '0.00',
+    artista VARCHAR(15) NULL DEFAULT NULL,
+    id INT PRIMARY KEY AUTO_INCREMENT
 );
 
 CREATE TABLE cliente(
-cliente_id INT(11) NOT NULL,
-nome VARCHAR(50) NULL DEFAULT NULL,
-twitter_user VARCHAR(50) NULL DEFAULT NULL,
-email VARCHAR(50) NULL DEFAULT NULL,
-endereco VARCHAR(50) NULL DEFAULT NULL,
-cidade VARCHAR(50) NULL DEFAULT NULL,
-cep INT(8) NULL DEFAULT NULL,
-telefone INT(15) NULL DEFAULT NULL
+    cliente_id INT(11) NOT NULL,
+    nome VARCHAR(50) NULL DEFAULT NULL,
+    twitter_user VARCHAR(50) NULL DEFAULT NULL,
+    email VARCHAR(50) NULL DEFAULT NULL,
+    endereco VARCHAR(50) NULL DEFAULT NULL,
+    cidade VARCHAR(50) NULL DEFAULT NULL,
+    cep INT(8) NULL DEFAULT NULL,
+    telefone INT(15) NULL DEFAULT NULL
 );
 
 CREATE TABLE compra(
-compra_id INT(11) NULL DEFAULT NULL,
-data_compra DATE NULL DEFAULT NULL,
-data_entrega DATE NULL DEFAULT NULL,
-frete int(10) NULL DEFAULT NULL,
-cod_rastreio INT(15) NULL DEFAULT NULL,
-pagamento INT(10) NULL DEFAULT NULL,
-reserva INT(10) NULL DEFAULT NULL,
-quantidade INT(10) NULL DEFAULT NULL,
-preco_total INT(10) NULL DEFAULT NULL
+    compra_id INT(11) NULL DEFAULT NULL,
+    data_compra DATE NULL DEFAULT NULL,
+    data_entrega DATE NULL DEFAULT NULL,
+    frete int(10) NULL DEFAULT NULL,
+    cod_rastreio INT(15) NULL DEFAULT NULL,
+    pagamento INT(10) NULL DEFAULT NULL,
+    reserva INT(10) NULL DEFAULT NULL,
+    quantidade INT(10) NULL DEFAULT NULL,
+    preco_total INT(10) NULL DEFAULT NULL
 );
 
 CREATE TABLE estoque(
-produto_id INT(11) NOT NULL,
-quantidade INT(10) NOT NULL,
-preco DECIMAL(9,2) NULL DEFAULT '0.00'
+    produto_id INT(11) NOT NULL,
+    quantidade INT(10) NOT NULL,
+    preco DECIMAL(9,2) NULL DEFAULT '0.00'
 );
 
 CREATE TABLE rastreio(
-cod_rastreio INT(15) NULL DEFAULT NULL,
-produto_id INT(11) NOT NULL,
-status CHAR(1) NOT NULL DEFAULT 'A',
-descricao_status VARCHAR(50) NULL DEFAULT NULL
+    cod_rastreio INT(15) NULL DEFAULT NULL,
+    produto_id INT(11) NOT NULL,
+    status CHAR(1) NOT NULL DEFAULT 'A',
+    descricao_status VARCHAR(50) NULL DEFAULT NULL
 );
 
 CREATE TABLE postagem(
-postagem_id INT(15) NULL DEFAULT NULL,
-forms_id INT(15) NULL DEFAULT NULL,
-data_post DATE NULL DEFAULT NULL,
-link_post VARCHAR(50) NULL DEFAULT NULL,
-n_compras INT(10) NULL DEFAULT NULL
+    produtoid INT NOT NULL,
+	formsid INT NOT NULL,
+    datapost DATETIME, 
+    linkpost VARCHAR(100)CHARACTER SET utf8 NOT NULL,
+    numcompras INT(10) NULL DEFAULT NULL
+    PRIMARY KEY (produtoid)
 );
 
 CREATE TABLE formulario(
-forms_id INT(15) NULL DEFAULT NULL,
-link VARCHAR(50) NULL DEFAULT NULL,
-perguntas VARCHAR(100) NULL DEFAULT NULL
+    formsid INT NOT NULL,
+    link VARCHAR(100)CHARACTER SET utf8 NOT NULL,
+    perguntas VARCHAR (300) CHARACTER SET utf8 NOT NULL, 
+    PRIMARY KEY (formsid)
 );
